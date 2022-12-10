f24bb40028e111211acccc5199fc889ba54116e0
DudsTheKid3756 <curlyq3756@gmail.com>
2022-12-09 22:45:51 -0500

    fixed crazy eights suite change issue

--- main/setup/card_games/deck.py
+++ main/setup/card_games/deck.py
@@ -12,7 +12,7 @@ class Deck:
         with open("C:/Users/duds_the_kid_3756/Desktop/Code/python_games/main/setup/card_games/strings.txt") as f:
             lines = f.readlines()
 
-        suites = [lines[i].strip() for i in range(1, 5)]
+        self.suites = [lines[i].strip() for i in range(1, 5)]
         words = [lines[i].strip() for i in range(7, 20)]
         values = [(words.index(word) + 1, word) for word in words]
-        self.deck = [self.Card(suite, value) for suite in suites for value in values]
+        self.deck = [self.Card(suite, value) for suite in self.suites for value in values]
